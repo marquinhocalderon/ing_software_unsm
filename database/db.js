167ee8b4afaf8b73c6bcccfaa13627f2aed21f0b
marquitocalderon <marco.calderon1701@hotmail.com>
2023-06-09 18:37:45 -0500

    prueba ilimitado

--- database/db.js
+++ database/db.js
@@ -2,13 +2,14 @@ const mysql = require('mysql2'); // o 'mysql' si prefieres
 
 // Crea el pool de conexiones
 const pool = mysql.createPool({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USERNAME, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: Infinity,
   queueLimit: 0
+  
 });
 // Exporta el pool para que pueda ser utilizado desde otros módulos
 module.exports = pool;
