29ca2817b0db857c3a58aa949c650bd2513f919d
marquitocalderon <marco.calderon1701@hotmail.com>
2023-06-09 18:02:12 -0500

    prueba base

--- database/db.js
+++ database/db.js
@@ -2,11 +2,13 @@ const mysql = require('mysql2'); // o 'mysql' si prefieres
 
 // Crea el pool de conexiones
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  port: 3306,
-  database: 'proyecto'
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 // Exporta el pool para que pueda ser utilizado desde otros módulos
 module.exports = pool;
