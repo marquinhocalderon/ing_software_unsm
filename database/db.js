c02bca32833a418436ce28008dcdfd6e5772489f
marquitocalderon <marco.calderon1701@hotmail.com>
2023-06-09 18:42:41 -0500

    prueba2

--- database/db.js
+++ database/db.js
@@ -2,13 +2,14 @@ const mysql = require('mysql2'); // o 'mysql' si prefieres
 
 // Crea el pool de conexiones
 const pool = mysql.createPool({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USERNAME, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
   waitForConnections: true,
-  connectionLimit: 1000,
+  connectionLimit: 999999, // Establece un límite de conexiones muy alto
   queueLimit: 0
+  
 });
 // Exporta el pool para que pueda ser utilizado desde otros módulos
 module.exports = pool;
