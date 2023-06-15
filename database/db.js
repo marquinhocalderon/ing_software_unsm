f9931b1d014f4805fd98d44b53086826b230e63d
marquitocalderon <marquitocalderon>
2023-06-15 10:37:54 -0500

    gaa

--- database/db.js
+++ database/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2'); // o 'mysql' si prefieres
 
 // Crea el pool de conexiones
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'buh5pev0wmpkxzzzunkj-mysql.services.clever-cloud.com',
+  user: 'uxsvg0q0r58hniyu',
+  password: 'BOHAFwBGKvil2t1JoGSO',
   port: 3306,
-  database: 'proyecto'
+  database: 'buh5pev0wmpkxzzzunkj'
 });
 // Exporta el pool para que pueda ser utilizado desde otros módulos
 module.exports = pool;
